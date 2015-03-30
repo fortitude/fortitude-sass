0b253b836dfaa102569f75e1b19dc31b4fcef2eb
Aaron Reisman <aaron@hired.com>
2015-03-30 11:30:16 -0700

    Cleanup fix for viewport scaling on iOS
    also bump version to 0.7.1

--- app/assets/javascripts/fortitude/generic/viewport.js
+++ app/assets/javascripts/fortitude/generic/viewport.js
@@ -4,18 +4,14 @@
   // zoom fix on input elements
   $(document).on('focus blur', ':input', function(event) {
     var $viewport = $('meta[name="viewport"]'),
-        scale = 1;
-    
+        content = 'width=device-width, initial-scale=1.0';
+
     if(event.type === 'focusout' ||
        event.type === 'blur'){
-      
-      scale = 5;
+
+      content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0';
     }
-    
-    $viewport.attr('content', 
-      'width=device-width,' +
-      'initial-scale=1,' +
-      'maximum-scale=' + scale
-    );
+
+    $viewport.attr('content', content);
   });
 })(jQuery);
