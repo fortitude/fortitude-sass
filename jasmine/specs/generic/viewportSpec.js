18e5724a45e6c8ab3c3393257a056b5112bd32df
Aaron Reisman <aaron@hired.com>
2015-03-30 11:30:16 -0700

    Cleanup fix for viewport scaling on iOS
    also bump version to 0.7.1

--- jasmine/specs/generic/viewportSpec.js
+++ jasmine/specs/generic/viewportSpec.js
@@ -2,30 +2,27 @@
   'use strict';
 
   describe('the input zoom fix', function(){
-    var contentVal = 'width=device-width,' +
-          'initial-scale=1,' +
-          'maximum-scale=',
-        $viewport, $input;
-    
+    var $viewport, $input;
+
     beforeEach(function(){
       loadFixtures('viewportFixture.html');
       $viewport = $('meta[name=viewport]');
       $input = $('input');
     });
-    
+
     it('fixes the zoom when input focused', function(){
       $input.trigger('focus');
-      expect($viewport.attr('content')).toEqual(contentVal + '1');
+      expect($viewport.attr('content')).toEqual('width=device-width, initial-scale=1.0');
     });
 
     it('returns zoom to normal when input focused out', function(){
       $input.trigger('focusout');
-      expect($viewport.attr('content')).toEqual(contentVal + '5');
+      expect($viewport.attr('content')).toEqual('width=device-width, initial-scale=1.0, maximum-scale=1.0');
     });
 
     it('returns zoom to normal when input blurred', function(){
       $input.trigger('blur');
-      expect($viewport.attr('content')).toEqual(contentVal + '5');
+      expect($viewport.attr('content')).toEqual('width=device-width, initial-scale=1.0, maximum-scale=1.0');
     });
   });
 
